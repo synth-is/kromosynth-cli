302f0fd9f7105b492681e1656f2bf1a9314202f7
Björn Þór Jónsson <bangsi@bthj.is>
2024-01-15 10:23:26 +0100

    Updates to evaluation web socket server test calls.

--- cli-app/test/test-evaluate.js
+++ cli-app/test/test-evaluate.js
@@ -1,9 +1,10 @@
 import WebSocket from "ws";
 
-const evaluationServerHost = 'ws://localhost:8081';
+const featureExtractionServerHost = 'ws://localhost:8081';
+const qualityEvaluationServerHost = 'ws://localhost:8082';
+const diversityEvaluationServerHost = 'ws://localhost:8083';
 
-const webSocket = new WebSocket(evaluationServerHost);
-webSocket.binaryType = "arraybuffer"; // Set binary type for receiving array buffers
+const SAMPLE_RATE = 16000;
 
 // Function to generate a random audio buffer (mock data)
 function generateRandomSoundBuffer(length) {
@@ -16,48 +17,109 @@ function generateRandomSoundBuffer(length) {
   return buffer;
 }
 
-// send websocket message to server, with audio buffer
-export async function getAudioClassPredictionsFromWebsocket(
-  featureMap,
+// send websocket message to server, with audio buffer and receive features
+function getFeaturesFromWebsocket(
   audioBufferChannelData,
-  geneEvaluationWebsocketServerHost
 ) {
+  const webSocket = new WebSocket(featureExtractionServerHost);
+  webSocket.binaryType = "arraybuffer"; // Set binary type for receiving array buffers
   return new Promise((resolve, reject) => {
     webSocket.on("open", () => {
-      webSocket.send(JSON.stringify(featureMap));
       webSocket.send(audioBufferChannelData);
     });
     webSocket.on("message", (message) => {
-      const predictions = JSON.parse(message);
-      resolve(predictions);
+      const features = JSON.parse(message);
+      resolve(features);
     });
     webSocket.on("error", (error) => {
-      delete clients[geneEvaluationWebsocketServerHost];
       reject(error);
     });
   });
-
 }
 
-// generate feature map (mock data)
-function generateFeatureMap() {
-  return {
-    "class1": [0.1, 0.2, 0.3],
-    "class2": [0.4, 0.5, 0.6],
-    "class3": [0.7, 0.8, 0.9],
-  };
+// send websocket message to server, with audio buffer and receive quality
+function getQualityFromWebsocket(
+  audioBufferChannelData,
+) {
+  const webSocket = new WebSocket(qualityEvaluationServerHost);
+  webSocket.binaryType = "arraybuffer"; // Set binary type for receiving array buffers
+  return new Promise((resolve, reject) => {
+    webSocket.on("open", () => {
+      webSocket.send(audioBufferChannelData);
+    });
+    webSocket.on("message", (message) => {
+      const quality = JSON.parse(message);
+      resolve(quality);
+    });
+    webSocket.on("error", (error) => {
+      reject(error);
+    });
+  });
 }
 
-export async function callEvaluationServiceWithARandomSoundBuffer() {
-  const featureMap = generateFeatureMap();
-  const audioBuffer = generateRandomSoundBuffer();
-  const predictions = await getAudioClassPredictionsFromWebsocket(
-    featureMap,
-    audioBuffer,
-    evaluationServerHost
+// send websocket message to server, with feature vectors and fitness values and receive diversity
+function getDiversityFromWebsocket(
+  featureVectors,
+  fitnessValues,
+) {
+  const webSocket = new WebSocket(diversityEvaluationServerHost);
+  return new Promise((resolve, reject) => {
+    webSocket.on("open", () => {
+      const diversityMessage = {
+        "feature_vectors": featureVectors,
+        "fitness_values": fitnessValues,
+      };
+      webSocket.send(JSON.stringify(diversityMessage));
+    });
+    webSocket.on("message", (message) => {
+      const diversity = JSON.parse(message);
+      resolve(diversity);
+    });
+    webSocket.on("error", (error) => {
+      reject(error);
+    });
+  });
+}
+
+async function callFeatureExtractionService( audioBuffer ) {
+  const _audioBuffer = audioBuffer || generateRandomSoundBuffer(SAMPLE_RATE);
+  // console.log('audio buffer:',_audioBuffer);
+  const features = await getFeaturesFromWebsocket(_audioBuffer);
+  // console.log('audio features:', features);
+  return features.features;
+}
+
+async function callQualityEvaluationService( audioBuffer ) {
+  const _audioBuffer = audioBuffer || generateRandomSoundBuffer(SAMPLE_RATE);
+  // console.log('audio buffer:', _audioBuffer);
+  const quality = await getQualityFromWebsocket(_audioBuffer);
+  // console.log('audio quality:', quality);
+  return quality.fitness;
+}
+
+async function callDiversityEvaluationServiceWithFeatureVectorsAndFitnessValues( numberOfEvaluationCandidates = 1 ) {
+  // get numberOfEvaluationCandidates feature vectors and fitness values by calling callFeatureExtractionServiceWithARandomSoundBuffer and callQualityEvaluationServiceWithARandomSoundBuffer
+  const featureVectors = [];
+  const fitnessValues = [];
+  for (let i = 0; i < numberOfEvaluationCandidates; i++) {
+    const audioBuffer = generateRandomSoundBuffer(SAMPLE_RATE);
+    const featureVector = await callFeatureExtractionService( audioBuffer );
+    const fitnessValue = await callQualityEvaluationService( audioBuffer );
+    featureVectors.push(featureVector);
+    fitnessValues.push(fitnessValue);
+  }
+  // console.log('feature vectors:', featureVectors);
+  // console.log('fitness values:', fitnessValues);
+  const diversity = await getDiversityFromWebsocket(
+    featureVectors,
+    fitnessValues,
+    diversityEvaluationServerHost
   );
-  console.log('predictions:', predictions);
-  return predictions;
+  console.log('audio diversity:', diversity);
+  return diversity;
 }
 
-callEvaluationServiceWithARandomSoundBuffer();
+// callFeatureExtractionService();
+// callQualityEvaluationService();
+
+callDiversityEvaluationServiceWithFeatureVectorsAndFitnessValues(30);
