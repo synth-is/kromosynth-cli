2ef8b744177bad5b5ecb637f9fc9aa12a4a08a70
Björn Þór Jónsson <bangsi@bthj.is>
2024-02-22 23:28:38 +0100

    HPC configuration examples update

---
+++ HPC/evoruns-conf-examples/conf-quality/sbatch-all_quality-problems.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+FEATURES_SCRIPT="/home/bthj/kromosynth-evaluate/evaluation/unsupervised/features_mfcc.py --sample-rate 16000 --host localhost --force-host true --port 31051"
+QUALITY_SCRIPT="/home/bthj/kromosynth-evaluate/evaluation/unsupervised/quality_problems.py --sample-rate 16000 --host localhost --force-host true --port 32051 --quality-methods 'click_count_percentage,discontinuity_count_percentage,gaps_count_percentage,saturation_percentage,true_peak_clipping_percentage,noise_burst_percentage,compressibility_percentage'"
+PROJECTION_SCRIPT="/home/bthj/kromosynth-evaluate/evaluation/unsupervised/projection_pca_quantised.py --dimensions 2 --dimension-cells 50 --host localhost --force-host true --port 33051"
+VARIATION_SERVER_COUNT=4
+RENDERING_SERVER_COUNT=4
+FEATURES_SERVER_COUNT=4
+QUALITY_SERVER_COUNT=4
+PROJECTION_SERVER_COUNT=4
+TOTAL_SERVER_COUNT=$((RENDERING_SERVER_COUNT + FEATURES_SERVER_COUNT + QUALITY_SERVER_COUNT + PROJECTION_SERVER_COUNT + 1)) # +1 for the controller
+BATCH_COUNT=1
+
+/home/bthj/QD/conf-quality/sbatch.sh /home/bthj/QD/conf-quality/evolution-runs_mfcc_pca-quantised_quality-problems.jsonc --features-script "${FEATURES_SCRIPT}" --quality-script "${QUALITY_SCRIPT}" --projection-script "${PROJECTION_SCRIPT}" --variation-server-count "${VARIATION_SERVER_COUNT}" --rendering-server-count "${RENDERING_SERVER_COUNT}" --features-server-count "${FEATURES_SERVER_COUNT}" --quality-server-count "${QUALITY_SERVER_COUNT}" --projection-server-count "${PROJECTION_SERVER_COUNT}" --total-server-count "${TOTAL_SERVER_COUNT}" --batch-count "${BATCH_COUNT}"
+/home/bthj/QD/conf-quality/sbatch.sh /home/bthj/QD/conf-quality/evolution-runs_mfcc_pca-quantised_quality-problems_parents-max-1_scoreRamp.jsonc --features-script "${FEATURES_SCRIPT}" --quality-script "${QUALITY_SCRIPT}" --projection-script "${PROJECTION_SCRIPT}" --variation-server-count "${VARIATION_SERVER_COUNT}" --rendering-server-count "${RENDERING_SERVER_COUNT}" --features-server-count "${FEATURES_SERVER_COUNT}" --quality-server-count "${QUALITY_SERVER_COUNT}" --projection-server-count "${PROJECTION_SERVER_COUNT}" --total-server-count "${TOTAL_SERVER_COUNT}" --batch-count "${BATCH_COUNT}"
+/home/bthj/QD/conf-quality/sbatch.sh /home/bthj/QD/conf-quality/evolution-runs_mfcc_pca-quantised_quality-problems_parents-max-10_scoreRamp.jsonc --features-script "${FEATURES_SCRIPT}" --quality-script "${QUALITY_SCRIPT}" --projection-script "${PROJECTION_SCRIPT}" --variation-server-count "${VARIATION_SERVER_COUNT}" --rendering-server-count "${RENDERING_SERVER_COUNT}" --features-server-count "${FEATURES_SERVER_COUNT}" --quality-server-count "${QUALITY_SERVER_COUNT}" --projection-server-count "${PROJECTION_SERVER_COUNT}" --total-server-count "${TOTAL_SERVER_COUNT}" --batch-count "${BATCH_COUNT}"
+/home/bthj/QD/conf-quality/sbatch.sh /home/bthj/QD/conf-quality/evolution-runs_mfcc_pca-quantised_quality-problems_parents-max-10_noScoreRamp.jsonc --features-script "${FEATURES_SCRIPT}" --quality-script "${QUALITY_SCRIPT}" --projection-script "${PROJECTION_SCRIPT}" --variation-server-count "${VARIATION_SERVER_COUNT}" --rendering-server-count "${RENDERING_SERVER_COUNT}" --features-server-count "${FEATURES_SERVER_COUNT}" --quality-server-count "${QUALITY_SERVER_COUNT}" --projection-server-count "${PROJECTION_SERVER_COUNT}" --total-server-count "${TOTAL_SERVER_COUNT}" --batch-count "${BATCH_COUNT}"
--- HPC/evoruns-conf-examples/conf-quality/sbatch.sh
+++ HPC/evoruns-conf-examples/conf-quality/sbatch.sh
@@ -5,7 +5,68 @@ config_file_path=$1
 # set file name as variable
 file_name=$(basename -- "$config_file_path")
 
-for iteration in {1..1}; do
+while [[ $# -gt 1 ]] ; do
+    key="$2"
+    case $key in
+        --features-script)
+        features_script="$3"
+        shift
+        ;;
+        --quality-script)
+        quality_script="$3"
+        shift
+        ;;
+        --projection-script)
+        projection_script="$3"
+        shift
+        ;;
+        --variation-server-count)
+        variation_server_count="$3"
+        shift
+        ;;
+        --rendering-server-count)
+        rendering_server_count="$3"
+        shift
+        ;;
+        --features-server-count)
+        features_server_count="$3"
+        shift
+        ;;
+        --quality-server-count)
+        quality_server_count="$3"
+        shift
+        ;;
+        --projection-server-count)
+        projection_server_count="$3"
+        shift
+        ;;
+        --total-server-count)
+        total_server_count="$3"
+        shift
+        ;;
+        --batch-count)
+        batch_count="$3"
+        shift
+        ;;
+        *)
+        ;;
+    esac
+    shift
+done
+
+echo "features_script: $features_script"
+echo "quality_script: $quality_script"
+echo "projection_script: $projection_script"
+echo "variation_server_count: $variation_server_count"
+echo "rendering_server_count: $rendering_server_count"
+echo "features_server_count: $features_server_count"
+echo "quality_server_count: $quality_server_count"
+echo "projection_server_count: $projection_server_count"
+echo "total_server_count: $total_server_count"
+echo "batch_count: $batch_count"
+
+# for iteration in {1..1}}; do
+for (( iteration=1; iteration<=$batch_count; iteration++ )); do
 
     # Generate the SLURM script to be submitted
     # "EOF" means "end of file"
@@ -14,18 +75,18 @@ for iteration in {1..1}; do
 
 #SBATCH --job-name=${file_name}
 #SBATCH --output=/home/bthj/slurm-output/${file_name}-${iteration}.out
-#SBATCH --ntasks=31
+#SBATCH --ntasks=${total_server_count}
 #SBATCH --mem-per-cpu=4G
 #SBATCH --cpus-per-task=1
 #SBATCH --dependency=singleton
-#SBATCH --time=03:00:30
+#SBATCH --time=01:00:30
 
 # Declare an associative array
 declare -A pids
 
 cd /home/bthj/kromosynth-cli/gRPC/
 # Loop to create gRPC server instances
-for ((i=1; i<=6; i++)); do
+for ((i=1; i<=${variation_server_count}; i++)); do
   # Generate a unique key using the loop index
   key="var_\${i}"
 
@@ -38,7 +99,7 @@ done
 
 cd /home/bthj/kromosynth-render/render-socket/
 # Loop to create genome rendering server instances
-for ((i=1; i<=6; i++)); do
+for ((i=1; i<=${rendering_server_count}; i++)); do
   # Generate a unique key using the loop index
   key="render_\${i}"
 
@@ -54,36 +115,36 @@ done
 cd /home/bthj/kromosynth-evaluate/ 
 
 # Loop to create evaluation-socket-server-features server instances
-for ((i=1; i<=6; i++)); do
+for ((i=1; i<=${features_server_count}; i++)); do
   # Generate a unique key using the loop index
   key="features_\${i}"
 
   rm /home/bthj/gRPC-hosts/evaluation-feature-socket-${file_name}-host-\$i
-  apptainer exec --mount 'type=bind,source=/home/bthj,destination=/home/bthj' --env PYTHONPATH=$PYTHONPATH:/home/bthj/kromosynth-evaluate /home/bthj/kromosynth-runner-python-CPU.sif python /home/bthj/kromosynth-evaluate/evaluation/unsupervised/features_mfcc.py --sample-rate 16000 --host localhost --force-host true --port 31051 --host-info-file /home/bthj/gRPC-hosts/evaluation-feature-socket-${file_name}-host-\$i &
+  apptainer exec --mount 'type=bind,source=/home/bthj,destination=/home/bthj' --env PYTHONPATH=$PYTHONPATH:/home/bthj/kromosynth-evaluate /home/bthj/kromosynth-runner-python-CPU.sif python ${features_script} --host-info-file /home/bthj/gRPC-hosts/evaluation-feature-socket-${file_name}-host-\$i &
 
   # Assign a value to the associative array using the unique key
   pids[\$key]="\$!"
 done
 
-# Loop to create evaluation-socket-server-quality-problems server instances
-for ((i=1; i<=6; i++)); do
+# Loop to create evaluation-socket-server-quality server instances
+for ((i=1; i<=${quality_server_count}; i++)); do
   # Generate a unique key using the loop index
   key="features_\${i}"
 
   rm /home/bthj/gRPC-hosts/evaluation-quality-socket-${file_name}-host-\$i
-  apptainer exec --mount 'type=bind,source=/home/bthj,destination=/home/bthj' --env PYTHONPATH=$PYTHONPATH:/home/bthj/kromosynth-evaluate /home/bthj/kromosynth-runner-python-CPU.sif python /home/bthj/kromosynth-evaluate/evaluation/unsupervised/quality_problems.py --sample-rate 16000 --host localhost --force-host true --port 32051 --host-info-file /home/bthj/gRPC-hosts/evaluation-quality-socket-${file_name}-host-\$i --quality-methods 'click_count_percentage,discontinuity_count_percentage,gaps_count_percentage,saturation_percentage,true_peak_clipping_percentage,noise_burst_percentage,compressibility_percentage' &
+  apptainer exec --mount 'type=bind,source=/home/bthj,destination=/home/bthj' --env PYTHONPATH=$PYTHONPATH:/home/bthj/kromosynth-evaluate /home/bthj/kromosynth-runner-python-CPU.sif python ${quality_script} --host-info-file /home/bthj/gRPC-hosts/evaluation-quality-socket-${file_name}-host-\$i &
 
   # Assign a value to the associative array using the unique key
   pids[\$key]="\$!"
 done
 
-# Loop to create evaluation-socket-server-projection_pca_quantised server instances
-for ((i=1; i<=6; i++)); do
+# Loop to create evaluation-socket-server-projection server instances
+for ((i=1; i<=${projection_server_count}; i++)); do
   # Generate a unique key using the loop index
   key="features_\${i}"
 
   rm /home/bthj/gRPC-hosts/evaluation-projection-socket-${file_name}-host-\$i
-  apptainer exec --mount 'type=bind,source=/home/bthj,destination=/home/bthj' --env PYTHONPATH=$PYTHONPATH:/home/bthj/kromosynth-evaluate /home/bthj/kromosynth-runner-python-CPU.sif python /home/bthj/kromosynth-evaluate/evaluation/unsupervised/projection_pca_quantised.py --dimensions 2 --dimension-cells 50 --host localhost --force-host true --port 33051 --host-info-file /home/bthj/gRPC-hosts/evaluation-projection-socket-${file_name}-host-\$i  &
+  apptainer exec --mount 'type=bind,source=/home/bthj,destination=/home/bthj' --env PYTHONPATH=$PYTHONPATH:/home/bthj/kromosynth-evaluate /home/bthj/kromosynth-runner-python-CPU.sif python ${projection_script} --host-info-file /home/bthj/gRPC-hosts/evaluation-projection-socket-${file_name}-host-\$i  &
 
   # Assign a value to the associative array using the unique key
   pids[\$key]="\$!"
