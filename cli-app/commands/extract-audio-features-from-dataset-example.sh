db2e7bb842f07cf1c1ef89debc432808f0c40328
Björn Þór Jónsson <bangsi@bthj.is>
2024-07-15 17:29:19 +0200

    updating audio feature extraction example

--- cli-app/commands/extract-audio-features-from-dataset-example.sh
+++ cli-app/commands/extract-audio-features-from-dataset-example.sh
@@ -6,4 +6,4 @@
 
 # node extract-audio-features-from-dataset.js /Users/bjornpjo/Downloads/OneBillionWav /Users/bjornpjo/Downloads/OneBillionWav_features 44100 /Users/bjornpjo/.cache/torch/hub/checkpoints 'ws://localhost:31051' "020.wav,030.wav,040.wav,050.wav,060.wav,070.wav,080.wav,090.wav,100.wav"
 
-kromosynth extract-features --dataset-folder /Users/bjornpjo/Downloads/evorenders --write-to-folder /Users/bjornpjo/Downloads/evofeatures --sample-rate 44100 --ckpt-dir /Users/bjornpjo/.cache/torch/hub/checkpoints --feature-extraction-server-host 'ws://localhost:31051' # --suffixes-filter "020.wav,030.wav,040.wav,050.wav,060.wav,070.wav,080.wav,090.wav,100.wav"
+kromosynth extract-features --dataset-folder /Users/bjornpjo/Downloads/evorenders --write-to-folder /Users/bjornpjo/Downloads/evofeatures --sample-rate 44100 --ckpt-dir /Users/bjornpjo/.cache/torch/hub/checkpoints --feature-extraction-server-host 'ws://localhost:31051' # --suffixes-filter "020.wav,030.wav,040.wav,050.wav,060.wav,070.wav,080.wav,090.wav,100.wav" --feature-types-filter "mfcc,mfcc-statistics"
