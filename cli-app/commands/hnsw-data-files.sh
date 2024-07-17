026233e49a0a22c53fb1d296029be3ec472cf1a1
Björn Þór Jónsson <bangsi@bthj.is>
2024-07-18 01:06:27 +0200

    hnsw data file preparation updates

---
+++ cli-app/commands/hnsw-data-files.sh
@@ -0,0 +1,2 @@
+# example command:
+# node cli-app/test/test-hnswlib-node.js mfcc /Users/bjornpjo/Downloads/audio-features/OneBillionWav_features_filtered /Users/bjornpjo/Downloads/hnsw-indexes/OneBillionWav_features_filtered_mfcc 3
